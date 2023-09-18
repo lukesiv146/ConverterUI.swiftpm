dc484b1508bdba4b75a6ff1542144e56a5f6eeff
Luke Sivulka <lukesiv146@icloud.com>
2023-09-18 11:02:17 -0500

    Made an opening for the app, will add buttons for each different converter.

---
+++ OpeningView.swift
@@ -0,0 +1,32 @@
+//
+//  OpeningView.swift
+//  ConverterUI
+//
+//  Created by Luke J. Sivulka on 9/18/23.
+//
+
+import SwiftUI
+
+struct OpeningView: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                
+                NavigationLink(destination: BinaryToDecimal()) {
+                    Text("BinaryDecimal")
+                        .font(.system(size: 24))
+                        .foregroundColor(.white)
+                        .frame(width: 150, height: 70)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                    
+                        .navigationBarTitle("Converter")
+                }
+                
+                
+            }
+        }
+        
+        
+    }
+}
