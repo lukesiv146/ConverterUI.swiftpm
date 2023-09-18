e392a55ce7884925be97edeaaef66ef4462a8f51
Luke Sivulka <lukesiv146@icloud.com>
2023-09-18 10:34:20 -0500

    Initial Commit

---
+++ RodrigoView.swift
@@ -0,0 +1,20 @@
+//
+//  RodrigoView.swift
+//  ConverterUI
+//
+//  Created by Luke J. Sivulka on 9/18/23.
+//
+
+import SwiftUI
+
+struct RodrigoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RodrigoView_Previews: PreviewProvider {
+    static var previews: some View {
+        RodrigoView()
+    }
+}
